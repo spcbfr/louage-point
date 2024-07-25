b2d021831590e946a8de1802b9a2134ea5a0af05
Yusuf Bouzekri <yusufbouzekri@gmail.com>
2024-07-25 14:43:27 +0100

    Revert "Update DB Structure"
    This reverts commit 765ae2209d1f251e17b0be211f8bd0a276db8004.

--- database/migrations/2024_07_24_200348_create_stations_table.php
+++ database/migrations/2024_07_24_200348_create_stations_table.php
@@ -14,8 +14,7 @@ return new class extends Migration
         Schema::create('stations', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->double('longitude');
-            $table->double('latitude');
+            $table->geography('location');
             $table->timestamps();
         });
     }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,12 +2,9 @@
 
 namespace Database\Seeders;
 
-use App\Models\Station;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\App;
-use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,14 +13,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        if (App::isLocal()) {
-            User::factory()->create([
-                'name' => 'Test User',
-                'email' => 'admin@admin.com',
-                'password' => Hash::make('admin'),
-            ]);
-            Station::factory(15)->create();
-        }
+        // User::factory(10)->create();
 
+        User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+        ]);
     }
 }
