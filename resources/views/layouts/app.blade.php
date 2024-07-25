c3293e2eacdfac548ae9b7787f7b5755990c52bf
Yusuf Bouzekri <yusufbouzekri@gmail.com>
2024-07-25 15:00:26 +0100

    Add filamentPHP forms

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,19 +2,28 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <meta name="application-name" content="{{ config('app.name') }}">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>{{ config('app.name') }}</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        <style>
+            [x-cloak] {
+                display: none !important;
+            }
+        </style>
+
+        @filamentStyles
+        @vite('resources/css/app.css')
     </head>
-    <body class="font-sans antialiased">
+
+    <body class="antialiased">
         <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
             @include('layouts.navigation')
 
@@ -32,5 +41,8 @@
                 {{ $slot }}
             </main>
         </div>
+
+        @filamentScripts
+        @vite('resources/js/app.js')
     </body>
 </html>
